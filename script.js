71f6863ba4d2213b928757329406740c8fc88e6e
TayJung <tay.jung29@gmail.com>
2022-06-15 00:03:46 -0300

    atividade finalizada

---
+++ script.js
@@ -0,0 +1,27 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for(let count=1; count<=10; count++){
+console.log(count)
+}
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let numero = 10;
+while(numero >= 1){
+    console.log(numero);
+    numero --;
+}
+
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+for(let impar=1; impar<=100; impar+=2){
+console.log(impar)
+}
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+
+for(let par=0; par<=100; par+=2){
+    console.log(par)
+}
